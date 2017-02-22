1cbc80684972f9213699c9530bb2b4b1caa94801
Aurelien Burget <aurelion@gmail.com>
2017-02-22 09:56:37 +0100

    adding admin interface for the 3 models

---
+++ app/admin/consultation.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Consultation do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  permit_params :estimated_price, :start_time, :end_time, :status, :user_id, :demand_id, :created_at, :updated_at
+end
---
+++ app/admin/demand.rb
@@ -0,0 +1,17 @@
+ActiveAdmin.register Demand do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  permit_params :first_name, :last_name, :address, :city, :zipcode, :phone, :email, :birthday, :sex, :lat, :long, :photo, :created_at, :updated_at, :request_time, :illness, :due_date
+end
