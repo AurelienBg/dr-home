c74bb3078d56b61d04341181a142afcdf0dd419a
Aurelien Burget <aurelion@gmail.com>
2017-02-23 17:35:38 +0100

    api working juuuust fine

--- app/jobs/routific_job.rb
+++ app/jobs/routific_job.rb
@@ -5,58 +5,16 @@ class RoutificJob < ApplicationJob
 
   # def perform(*args)  with or without args !
   def perform
-    puts "Starting Routific call"
-    visits = {
-            "order_1" => {
-              "start" => "9:00",
-              "end" => "12:00",
-              "duration" => 10,
-              "location" => {
-                "name" => "6800 Cambie",
-                "lat" => 49.227107,
-                "lng" => -123.1163085
-              }
-            }
-          }
+    # import data from ActiveRecords to build the input for Routific
 
-    # visits = {}
-    # demands_of_the_day = Demand.where(Date.today < due_date)
-    # n = 1
-    # demands_of_the_day.each do |demand|
-    #   visits['order_#{n}'] =  {
-    #           "start" => "9:00",
-    #           "end" => "19:00",
-    #           "duration" => 20,
-    #           "location" => {
-    #             "name" => demand.address,
-    #             "lat" => demand.latitude,
-    #             "lng" => demand.longitude
-    #           }
-    #         }
-    #         n += 1
-    # end
+    visits = {}
+    fleet = {}
 
-fleet = {
-  "vehicle_1" => {
-    "start_location" => {
-      "name" => "800 Kingsway",
-      "lat" => 49.2553636,
-      "lng" => -123.0873365
-    },
-    "end_location" => {
-      "name" => "800 Kingsway",
-      "lat" => 49.2553636,
-      "lng" => -123.0873365
-    },
-    "shift_start" => "8:00",
-    "shift_end" => "12:00"
-  }
-}
-
-data = {
-  visits: visits,
-  fleet: fleet
-}
+    buid_data_hash(visits, fleet)
+    data = {
+      visits: visits,
+      fleet: fleet
+    }
 
     # data = hash
     puts data
@@ -73,10 +31,76 @@ data = {
     puts "Ending Routific call"
   end
 
+  private
 
-# private
-#   def buid_data_hash
-#     ..
-#     @data = {}
-#   end
+  def buid_data_hash(visits, fleet)
+    # visits = {
+    #         "order_1" => {
+    #           "start" => "9:00",
+    #           "end" => "12:00",
+    #           "duration" => 10,
+    #           "location" => {
+    #             "name" => "6800 Cambie",
+    #             "lat" => 49.227107,
+    #             "lng" => -123.1163085
+    #           }
+    #         }
+    #       }
+
+
+    demands_of_the_day = Demand.where("due_date <= ?", Date.today)
+    n = 1
+    demands_of_the_day.each do |demand|
+      visits["order_#{n}"] =  {
+        "start" => "9:00",
+        "end" => "19:00",
+        "duration" => 20,
+        "location" => {
+          "name" => demand.address,
+          "lat" => demand.latitude,
+          "lng" => demand.longitude
+        }
+      }
+      n += 1
+    end
+
+    # fleet = {
+    #   "vehicle_1" => {
+    #     "start_location" => {
+    #       "name" => "800 Kingsway",
+    #       "lat" => 49.2553636,
+    #       "lng" => -123.0873365
+    #     },
+    #     "end_location" => {
+    #       "name" => "800 Kingsway",
+    #       "lat" => 49.2553636,
+    #       "lng" => -123.0873365
+    #     },
+    #     "shift_start" => "8:00",
+    #     "shift_end" => "12:00"
+    #   }
+    # }
+
+    users_of_the_day = User.all
+    n = 1
+    users_of_the_day.each do |user|
+      fleet["vehicle_#{n}"] =  {
+        "vehicle_1" => {
+          "start_location" => {
+            "name" => user.address,
+            "lat" => user.latitude,
+            "lng" => user.longitude
+          },
+          "end_location" => {
+            "name" => user.address,
+            "lat" => user.latitude,
+            "lng" => user.latitude,
+          },
+          "shift_start" => "8:00",
+          "shift_end" => "20:00"
+        }
+      }
+      n += 1
+    end
+  end
 end
