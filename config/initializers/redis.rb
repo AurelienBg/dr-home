210cc4abbcc6b585d5e3d708d7f9fe6413de5621
Aurelien Burget <aurelion@gmail.com>
2017-02-23 09:58:57 +0100

    creating one fake job to have a template

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -13,15 +13,3 @@ if url
   end
   $redis = Redis.new(:url => url)
 end
-UPDATE YOUR PROCFILE FILE
-
-# Procfile
-web: bundle exec puma -C config/puma.rb
-worker: bundle exec sidekiq -C config/sidekiq.yml
-Commit & deploy. Then go to your dashboard to set dynos.
-HEROKU CRON JOBS
-
-Use the Heroku Scheduler
-
-HAPPY ASYNCHRONIZING!
-
