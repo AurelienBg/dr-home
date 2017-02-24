15768d01149b2238fd3b854f09ddcf1c46cec76a
Ulysse Schwartz <schwartzulysse@gmail.com>
2017-02-24 15:34:11 +0100

    prod fix

--- app/jobs/parse_file_job.rb
+++ app/jobs/parse_file_job.rb
@@ -5,11 +5,11 @@ class ParseFileJob < ApplicationJob
 
   def perform
 
-  CSV.foreach("ExtractionMonoTable_CAT18_ToutePopulation_201702230745.csv") do |row|
-    # row is an array. For first iteration:
-    f = FrenchDoctor.new
-    f.carte_pro = row[0]
-    f.name = row[1]
-    f.save
+  # CSV.foreach("ExtractionMonoTable_CAT18_ToutePopulation_201702230745.csv") do |row|
+  #   # row is an array. For first iteration:
+  #   f = FrenchDoctor.new
+  #   f.carte_pro = row[0]
+  #   f.name = row[1]
+  #   f.save
   end
 end
