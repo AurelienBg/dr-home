c11970eedfc897e3773b14c4224a1ec89d1619eb
Ulysse Schwartz <schwartzulysse@gmail.com>
2017-02-27 13:14:48 +0100

    map directions set

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,15 @@ require 'json'
 require 'open-uri'
 
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :map ]
   before_action :check_if_user_profile_completed, only: :dashboard
 
   def home
     @user = current_user
+  end
+
+  def map
+
   end
 
   def dashboard
@@ -47,6 +51,6 @@ class PagesController < ApplicationController
     unless current_user.profile_completed?
       flash[:notice] = 'You must complete your profile first!'
       redirect_to edit_user_path(current_user)
-    end 
+    end
   end
 end
