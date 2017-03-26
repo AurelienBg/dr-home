04a9491d4fc9aface93656f0089373c7b77e2b79
Aurelien Burget <aurelion@gmail.com>
2017-03-26 15:49:31 +0200

    replaced KEY by TOKER for postmark in the smtp rb file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,9 +33,9 @@ Rails.application.configure do
 
   # User letter opener for Developement
   # to be removed for long Seed
-  letter_opener can be replaced by smtp
+  # letter_opener can be replaced by smtp
   # config.action_mailer.delivery_method = :letter_opener
-  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
