7a6aeba0ec4f436395c5d9ebbee397b4aad81441
Aurelien Burget <aurelion@gmail.com>
2017-03-28 10:15:06 +0200

    remove 1 comment line

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,10 +1,8 @@
 Rails.application.configure do
-  # User letter opener for Developement
-  # to be removed for long Seed
+  # User letter opener for Developement (to be removed for long Seed)
   # letter_opener can be replaced by smtp
   # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.delivery_method = :smtp
-
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
 
